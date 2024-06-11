ba98713acdc18a1b3d2778d157925c7b6ddec647
Zeeshan Siddiqui <z.siddiqui@calo.app>
2024-06-11 07:27:40 +0400

    added teaser page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div>
+    <main className="relative bg-black-100 justify-center items-center flex overflow-hidden mx-auto sm:px-10 px-5 w-[100vw] h-[100vh]">
+      <div className="m-auto">
         <Hero />
       </div>
     </main>
