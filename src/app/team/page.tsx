e714762f1c6d848527b6be53ad472c0cf09f148e
Zeeshan Siddiqui <z.siddiqui@calo.app>
2024-06-11 04:12:24 +0400

    added team base structure

---
+++ src/app/team/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function team() {
+  return (
+    <div>
+      <h1>This is EXOU team</h1>
+    </div>
+  );
+}
+
+export default team;
---
+++ src/app/team/zeeshan/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function zeeshan() {
+  return (
+    <div>
+      <h1>Hello, I am Zeeshan, the founder of EXOU</h1>
+    </div>
+  );
+}
+
+export default zeeshan;
