c5cca3a6348831780cd2b86f08f8ae4055d95dff
Zeeshan Siddiqui <z.siddiqui@calo.app>
2024-06-14 16:52:10 +0400

    hotfix to testing landingpage

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -6,25 +6,25 @@ import FeatureGrid3x3 from "@/components/FeatureGrid3x3";
 import GridWithFlexVerticalLayout from "@/components/GridWithFlexVerticalLO";
 // import ContactUsMain from "@/components/ContactUs"; // removed due to error
 
-export function LandingPage() {
-  return (
-    <main className="relative bg-black-100 flex flex-col overflow-hidden mx-auto sm:px-10 justify-center items-center px-5">
-      <div className="w-full h-[85vh]">
-        <Hero />
-      </div>
-      <div className="min-w-[90vw] lg:min-w-[80vw]">
-        <ScaleBusinessWithUs />
-      </div>
-      <div className="pt-40 z-10">
-        <FeatureGrid3x3 />
-      </div>
-      <div className="w-[80vw] pb-20 pt-36 z-10">
-        <GridWithFlexVerticalLayout />
-      </div>
-      <div className="h-96 bg-white-500">ok</div>
-    </main>
-  );
-}
+// export function LandingPage() {
+//   return (
+//     <main className="relative bg-black-100 flex flex-col overflow-hidden mx-auto sm:px-10 justify-center items-center px-5">
+//       <div className="w-full h-[85vh]">
+//         <Hero />
+//       </div>
+//       <div className="min-w-[90vw] lg:min-w-[80vw]">
+//         <ScaleBusinessWithUs />
+//       </div>
+//       <div className="pt-40 z-10">
+//         <FeatureGrid3x3 />
+//       </div>
+//       <div className="w-[80vw] pb-20 pt-36 z-10">
+//         <GridWithFlexVerticalLayout />
+//       </div>
+//       <div className="h-96 bg-white-500">ok</div>
+//     </main>
+//   );
+// }
 
 function TestingLandingPage() {
   return <div>Internal Link only</div>;
